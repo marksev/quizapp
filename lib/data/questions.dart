e6b6b828c5bb21d0d476a6028ee30a9d217e8857
marksev <33703765+marksev@users.noreply.github.com>
2025-12-20 18:22:11 +0200

    Create countries quiz experience

---
+++ lib/data/questions.dart
@@ -0,0 +1,64 @@
+import '../models/question.dart';
+
+const List<Question> questions = [
+  Question(
+    country: 'France',
+    options: ['Paris', 'Lyon', 'Marseille', 'Toulouse'],
+    correctAnswer: 'Paris',
+  ),
+  Question(
+    country: 'Brazil',
+    options: ['Rio de Janeiro', 'Brasília', 'São Paulo', 'Salvador'],
+    correctAnswer: 'Brasília',
+  ),
+  Question(
+    country: 'Japan',
+    options: ['Osaka', 'Sapporo', 'Tokyo', 'Kyoto'],
+    correctAnswer: 'Tokyo',
+  ),
+  Question(
+    country: 'Kenya',
+    options: ['Mombasa', 'Nairobi', 'Kisumu', 'Nakuru'],
+    correctAnswer: 'Nairobi',
+  ),
+  Question(
+    country: 'Germany',
+    options: ['Berlin', 'Hamburg', 'Munich', 'Frankfurt'],
+    correctAnswer: 'Berlin',
+  ),
+  Question(
+    country: 'Australia',
+    options: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+    correctAnswer: 'Canberra',
+  ),
+  Question(
+    country: 'Canada',
+    options: ['Toronto', 'Vancouver', 'Ottawa', 'Montreal'],
+    correctAnswer: 'Ottawa',
+  ),
+  Question(
+    country: 'Egypt',
+    options: ['Giza', 'Alexandria', 'Cairo', 'Luxor'],
+    correctAnswer: 'Cairo',
+  ),
+  Question(
+    country: 'Mexico',
+    options: ['Guadalajara', 'Cancún', 'Tijuana', 'Mexico City'],
+    correctAnswer: 'Mexico City',
+  ),
+  Question(
+    country: 'Norway',
+    options: ['Bergen', 'Oslo', 'Trondheim', 'Stavanger'],
+    correctAnswer: 'Oslo',
+  ),
+  Question(
+    country: 'Thailand',
+    options: ['Bangkok', 'Chiang Mai', 'Phuket', 'Pattaya'],
+    correctAnswer: 'Bangkok',
+  ),
+  Question(
+    country: 'Argentina',
+    options: ['Córdoba', 'Mendoza', 'Buenos Aires', 'Rosario'],
+    correctAnswer: 'Buenos Aires',
+  ),
+];
